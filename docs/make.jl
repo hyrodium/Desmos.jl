b8b5982951459a0e938b38fa81a8c75744e7a03a
hyrodium <hyrodium@gmail.com>
2023-03-27 00:25:10 +0900

    add initial documentation

---
+++ docs/make.jl
@@ -0,0 +1,27 @@
+using Documenter
+using Desmos
+
+# Setup for doctests in docstrings
+DocMeta.setdocmeta!(Desmos, :DocTestSetup, :(using Desmos))
+
+makedocs(;
+    modules = [Desmos],
+    format = Documenter.HTML(
+        ansicolor=true,
+        canonical = "https://hyrodium.github.io/Desmos.jl/stable/",
+        assets = ["assets/favicon.ico", "assets/custom.css"],
+        edit_link="main",
+    ),
+    pages = [
+        "Home" => "index.md",
+    ],
+    repo = "https://github.com/hyrodium/Desmos.jl/blob/{commit}{path}#L{line}",
+    sitename = "Desmos.jl",
+    authors = "hyrodium <hyrodium@gmail.com>",
+)
+
+deploydocs(
+    repo = "github.com/hyrodium/Desmos.jl",
+    push_preview = true,
+    devbranch="main",
+)

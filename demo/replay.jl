8cb9b053ca9357a02d881830616b38e7b2a7ed6d
hyrodium <hyrodium@gmail.com>
2023-03-27 16:09:02 +0900

    add demo generation script with Replay.jl

---
+++ demo/replay.jl
@@ -0,0 +1,19 @@
+# Run this script with `julia --project=. replay.jl`
+using Replay
+using Desmos
+using JSON
+
+instructions = [
+    "using Desmos, JSON",
+    """
+    state = @desmos begin
+        @expression cos(x) color=RGB(1,0,0)
+        @expression sin(x) color=RGB(0,0,1)
+        tan(x)
+    end
+    """,
+    "",
+    "clipboard(JSON.json(state, 4))",
+]
+
+replay(instructions, use_ghostwriter=true)

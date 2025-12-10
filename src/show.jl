eaada1a52236147d7eb421c56880632742c9045a
Horikawa Yuto <hyrodium@gmail.com>
2025-12-10 16:10:13 +0900

    update show

--- src/show.jl
+++ src/show.jl
@@ -1,26 +1,15 @@
 function Base.show(io::IO, ::MIME"text/html", state::DesmosState)
-    # Desmos Calculator APIを使ったHTMLを生成
-    # TODO: update version
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    write(io, """
+        <div class="desmos-container" id="desmos-$(objectid(state))" style="width:600px;height:400px;"></div>
         <script src="https://www.desmos.com/api/v1.10/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script>
-    </head>
-    <body>
-        <div id="NAME" style="width: 800px; height: 400px;"></div>
         <script>
-            var elt = document.getElementById('NAME');
-            var calculator = Desmos.GraphingCalculator(elt);
-            state = $(JSON.json(state))
-            calculator.setState(state)
+            (function() {
+                var elt = document.getElementById("desmos-$(objectid(state))");
+                var calculator = Desmos.GraphingCalculator(elt);
+                calculator.setState($(JSON.json(state)));
+            })();
         </script>
-    </body>
-    </html>
-    """
-    write(io, html)
+    """)
 end
 
 function Base.show(io::IO, ::MIME"juliavscode/html", agif::DesmosState)

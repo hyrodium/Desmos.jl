7b69990ec2be956c66ca3ffa5666c3e813e83cb9
Horikawa Yuto <hyrodium@gmail.com>
2025-12-10 12:22:08 +0900

    update json_types.jl

--- src/json_types.jl
+++ src/json_types.jl
@@ -1,15 +1,15 @@
 abstract type AbstractDesmosExpression end
 
-@defaults struct DesmosColumn
+@kwarg struct DesmosColumn
     id::String
-    color::String
+    color::String = "#000000"
     values::Union{Vector{String}, Nothing} = nothing
     latex::Union{LaTeXString, Nothing} = nothing
     hidden::Union{Bool,Nothing} = nothing
 end
 JSON.omit_null(::Type{DesmosColumn}) = true
 
-@defaults struct DesmosSlider
+@kwarg struct DesmosSlider
     hard_min::Bool &(json=(name="hardMin",),)
     hard_max::Bool &(json=(name="hardMax",),)
     animation_period::Float64 &(json=(name="animationPeriod",),)
@@ -20,22 +20,22 @@ JSON.omit_null(::Type{DesmosColumn}) = true
 end
 JSON.omit_null(::Type{DesmosSlider}) = true
 
-@defaults struct DesmosParametricDomain
+@kwarg struct DesmosParametricDomain
     min::String
     max::String
 end
 JSON.omit_null(::Type{DesmosParametricDomain}) = true
 
-@defaults struct DesmosDomain
+@kwarg struct DesmosDomain
     min::String
     max::String
 end
 JSON.omit_null(::Type{DesmosDomain}) = true
 
-@defaults struct DesmosExpression <: AbstractDesmosExpression
-    type::String
+@kwarg struct DesmosExpression <: AbstractDesmosExpression
+    type::String = "expression"
     id::String
-    color::String
+    color::String = "#000000"
     latex::Union{LaTeXString, Nothing} = nothing
     slider::Union{DesmosSlider, Nothing} = nothing
     domain::Union{DesmosDomain, Nothing} = nothing
@@ -44,15 +44,15 @@ JSON.omit_null(::Type{DesmosDomain}) = true
 end
 JSON.omit_null(::Type{DesmosExpression}) = true
 
-@defaults struct DesmosTable <: AbstractDesmosExpression
-    type::String
+@kwarg struct DesmosTable <: AbstractDesmosExpression
+    type::String = "table"
     id::String
     columns::Vector{DesmosColumn}
 end
 JSON.omit_null(::Type{DesmosTable}) = true
 
-@defaults struct DesmosImage <: AbstractDesmosExpression
-    type::String
+@kwarg struct DesmosImage <: AbstractDesmosExpression
+    type::String = "image"
     id::String
     image_url::String
     name::String
@@ -61,44 +61,44 @@ JSON.omit_null(::Type{DesmosTable}) = true
 end
 JSON.omit_null(::Type{DesmosImage}) = true
 
-@defaults struct DesmosText <: AbstractDesmosExpression
-    type::String
+@kwarg struct DesmosText <: AbstractDesmosExpression
+    type::String = "text"
     id::String
     text::Union{String, Nothing} = nothing
     folder_id::Union{String, Nothing} = nothing &(json=(name="folderId",),)
 end
 JSON.omit_null(::Type{DesmosText}) = true
 
-@defaults struct DesmosFolder <: AbstractDesmosExpression
-    type::String
+@kwarg struct DesmosFolder <: AbstractDesmosExpression
+    type::String = "folder"
     id::String
     title::Union{String, Nothing} = nothing
 end
 JSON.omit_null(::Type{DesmosFolder}) = true
 
-@defaults struct DesmosExpressions
+@kwarg struct DesmosExpressions
     list::Vector{AbstractDesmosExpression}
 end
 
-@defaults struct DesmosViewport
+@kwarg struct DesmosViewport
     xmin::Float64
     ymin::Float64
     xmax::Float64
     ymax::Float64
 end
 
-@defaults struct DesmosGraph
-    viewport::DesmosViewport
+@kwarg struct DesmosGraph
+    viewport::Union{DesmosViewport, Nothing} = nothing
     user_locked_viewport::Union{Bool,Nothing} = nothing &(json=(name="userLockedViewport",),)
     complex::Union{Bool,Nothing} = nothing
 end
 JSON.omit_null(::Type{<:DesmosGraph}) = true
 
-@defaults struct DesmosState
-    graph::DesmosGraph
-    random_seed::String &(json=(name="randomSeed",),)
-    expressions::DesmosExpressions
+@kwarg struct DesmosState
     version::Int = 11
+    random_seed::String = "00000000000000000000000000000000" &(json=(name="randomSeed",),)
+    graph::DesmosGraph = DesmosGraph()
+    expressions::DesmosExpressions
     include_function_parameters_in_random_seed::Bool = true &(json=(name="includeFunctionParametersInRandomSeed",),)
     do_not_migrate_movable_point_style::Bool = true &(json=(name="doNotMigrateMovablePointStyle",),)
 end

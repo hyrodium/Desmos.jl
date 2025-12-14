b4b03ea88ed9e9a545fd04f772ee3530aedd76c2
Horikawa Yuto <hyrodium@gmail.com>
2025-12-14 23:18:32 +0900

    add support for more fields

--- test/runtests.jl
+++ test/runtests.jl
@@ -83,12 +83,19 @@ end
 end
 
 @testset "JSON I/O" begin
-    @testset "example$i.json" for i in 1:5
+    @testset "example$i.json" for i in 1:6
         path_json = joinpath(@__DIR__, "json_example", "example$i.json")
         state = JSON.parse(read(path_json, String), DesmosState)
         jo1 = JSON.parse(read(path_json, String))
         jo2 = JSON.parse(JSON.json(state, 4))
         delete!(jo1.graph, "__v12ViewportLatexStash")
+        for expression in jo1.expressions.list
+            if "columns" in keys(expression)
+                for column in expression.columns
+                    delete!(column, "__stashed_V12PointStyle")
+                end
+            end
+        end
         @test jo2 == jo1
     end
 end

4da291309bfc6e74ee0811f74845abda606da4aa
Yuto Horikawa <hyrodium@gmail.com>
2025-12-23 22:45:09 +0900

    update minimum auto width (#88)

--- src/show.jl
+++ src/show.jl
@@ -2,7 +2,7 @@ function Base.show(io::IO, ::MIME"text/html", state::DesmosState)
     config = get_desmos_display_config()
     obj_id = objectid(state)
 
-    width_style = config.width == 0 ? "width:100%;max-width:100%;min-width:600px;" : "width:$(config.width)px;"
+    width_style = config.width == 0 ? "width:100%;max-width:100%;min-width:400px;" : "width:$(config.width)px;"
     html = """
         <div class="desmos-container" id="desmos-$(obj_id)" style="$(width_style)height:$(config.height)px;"></div>
     """
